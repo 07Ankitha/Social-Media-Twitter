1cb1810bb98420c17847a1c5942fbbdf33f692a3
07Ankitha <156890727+07Ankitha@users.noreply.github.com>
2024-04-30 11:09:45 +0530

    Update connection.php

--- database/connection.php
+++ database/connection.php
@@ -1,1 +1,17 @@
+<?php
+// useless connection before i try to use class 
+
+// $servername = "localhost";
+// $db_name="twitter";
+// $username = "root";
+// $password = "";
+
+// try {
+//   $pdo = new PDO("mysql:host=$servername;dbname=$db_name", $username, $password);
+//   // set the PDO error mode to exception
+//   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// //   echo "Connected successfully";
+// } catch(PDOException $e) {
+//   echo "Connection failed: " . $e->getMessage();
+// }
 
